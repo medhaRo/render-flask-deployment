f2abd805c1dc167dd7c49cffe037da345457f3ee
medhaRo <medha.roy@ebiw.com>
2025-03-11 14:56:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     # Use PORT from the environment variable
     import os
     port = int(os.environ.get("PORT", 5000))  # Default to 8080 if PORT is not set
-    app.run(host='0.0.0.0', port=port, debug=True)
+#    app.run(host='0.0.0.0', port=port, debug=True)
 
