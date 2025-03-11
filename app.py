f255361ce3af556f76e8adcc1b5fe85972642a79
medhaRo <medha.roy@ebiw.com>
2025-03-11 13:08:32 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from file_api import file_api
 
@@ -7,6 +8,7 @@ app = Flask(__name__)
 # Register blueprints
 app.register_blueprint(file_api)
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Use PORT from the environment variable
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
